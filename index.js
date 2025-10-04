2ecbf222aa71c64db258f952b35d73e41f848b9f
ᡣ𐭩 𝑫𝒆𝒗-𝒇𝒆𝒅𝒆𝒙𝒚𝒛 🌙 <219152977+Dev-fedexyz17@users.noreply.github.com>
2025-10-04 09:24:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -36,12 +36,12 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 let { say } = cfonts
 console.log(chalk.magentaBright('\n✨️ Iniciando...'))
-say('BOT-MD', {
+say('Nagi-Bot', {
 font: 'simple',
 align: 'left',
 gradient: ['green', 'white']
 })
-say('Made with love by Destroy', {
+say('Made with love by Dev-fedexyz', {
 font: 'console',
 align: 'center',
 colors: ['cyan', 'magenta', 'yellow']
