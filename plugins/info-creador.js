67ff6cdf7b8d15c68520eccb3bcc9ac17ebf3842
ᡣ𐭩 𝑫𝒆𝒗-𝒇𝒆𝒅𝒆𝒙𝒚𝒛 🌙 <219152977+Dev-fedexyz17@users.noreply.github.com>
2025-10-04 10:06:29 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,10 +1,10 @@
-let handler = async (m, { conn }) => {
-let vcard = `BEGIN:VCARD
+let handler = async (m, { conn}) => {
+let vcard1 = `BEGIN:VCARD
 VERSION:3.0
 FN:✨ ˚₊ ᴍ ᴀ ʀ ᴄ ᴋ・୧
 ORG:✨ ˚₊ ᴍ ᴀ ʀ ᴄ ᴋ・୧
 TITLE:Metatron Executioner of Michael
-EMAIL;type=INTERNET:brayanfree881@gmail.com 
+EMAIL;type=INTERNET:brayanfree881@gmail.com
 TEL;type=CELL;waid=573001533523:+573001533523
 ADR;type=WORK:;;2-chōme-7-5 Fuchūchō;marck;Osaka;594-0071;Japan
 URL;type=WORK:https://www.tiktok.com/@fantom_uwu_330
@@ -12,6 +12,21 @@ X-WA-BIZ-NAME:𝑴𝒂𝒓𝒄𝒌 𝑼𝒘𝒖
 X-WA-BIZ-DESCRIPTION:𝑵𝒂𝒈𝒊-𝑩𝒐𝒕 𝐛𝐲 𝑫𝒆𝒗-𝒉𝒂𝒔𝒉𝒊𝒓𝒂𝒎𝒂
 X-WA-BIZ-HOURS:Mo-Su 00:00-23:59
 END:VCARD`
+
+let vcard2 = `BEGIN:VCARD
+VERSION:3.0
+FN:✨ ˚₊ ʜ ᴀ ꜱ ʜ ɪ ʀ ᴀ ᴍ ᴀ・୧
+ORG:✨ ˚₊ ʜ ᴀ ꜱ ʜ ɪ ʀ ᴀ ᴍ ᴀ・୧
+TITLE:Dev & Co-Founder of Nagi-Bot
+EMAIL;type=INTERNET:hashirama.dev@gmail.com
+TEL;type=CELL;waid=5491156178758:+5491156178758
+ADR;type=WORK:;;Tokyo-to;HashiDev;Tokyo;100-0001;Japan
+URL;type=WORK:https://github.com/hashirama-dev
+X-WA-BIZ-NAME:𝑯𝒂𝒔𝒉𝒊𝒓𝒂𝒎𝒂 𝑫𝒆𝒗
+X-WA-BIZ-DESCRIPTION:𝑵𝒂𝒈𝒊-𝑩𝒐𝒕 𝐛𝐲 𝑯𝒂𝒔𝒉𝒊𝒓𝒂𝒎𝒂
+X-WA-BIZ-HOURS:Mo-Su 00:00-23:59
+END:VCARD`
+
 let qkontak = {
 key: {
 fromMe: false,
@@ -20,15 +35,16 @@ remoteJid: "status@broadcast"
 },
 message: {
 contactMessage: {
-displayName: "𝑴𝒂𝒓𝒄𝒌 𝑼𝒘𝒖",
-vcard
+displayName: "𝑵𝒂𝒈𝒊-𝑩𝒐𝒕 𝐂𝐫𝐞𝐚𝐭𝐨𝐫𝐬",
+vcard: vcard1
 }
 }
 }
+
 await conn.sendMessage(m.chat, {
 contacts: {
-displayName: '𝑴𝒂𝒓𝒄𝒌 𝑼𝒘𝒖',
-contacts: [{ vcard }]
+displayName: '𝑵𝒂𝒈𝒊-𝑩𝒐𝒕 𝐂𝐫𝐞𝐚𝐭𝐨𝐫𝐬',
+contacts: [{ vcard: vcard1}, { vcard: vcard2}]
 },
 contextInfo: {
 externalAdReply: {
@@ -39,7 +55,7 @@ mediaType: 1,
 renderLargerThumbnail: true
 }
 }
-}, { quoted: qkontak })
+}, { quoted: qkontak})
 }
 
 handler.help = ['owner']
