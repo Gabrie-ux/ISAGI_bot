4b603908683f618548736ccd8235e5e0fbb20b76
ᡣ𐭩 𝑫𝒆𝒗-𝒇𝒆𝒅𝒆𝒙𝒚𝒛 🌙 <219152977+Dev-fedexyz17@users.noreply.github.com>
2025-10-05 14:04:40 -0300

    Update _validCommand.js

--- plugins/_validCommand.js
+++ plugins/_validCommand.js
@@ -1,59 +1,26 @@
-import fs from 'fs'
-
-let handler = async (m, { command, usedPrefix, plugins, conn}) => {
-  const comandoBuscado = usedPrefix + command
-
-  const existe = Array.isArray(plugins) && plugins.some(plugin => {
-    if (!plugin.command) return false
-
-    if (plugin.command instanceof RegExp) {
-      return plugin.command.test(command) || plugin.command.test(comandoBuscado)
+export async function before(m, { groupMetadata }) {
+if (!m.text || !global.prefix.test(m.text)) return
+const usedPrefix = global.prefix.exec(m.text)[0]
+const command = m.text.slice(usedPrefix.length).trim().split(' ')[0].toLowerCase()
+if (!command || command.length === 0) return
+const validCommand = (command, plugins) => {
+for (let plugin of Object.values(plugins)) {
+if (plugin.command && (Array.isArray(plugin.command) ? plugin.command : [plugin.command]).includes(command)) {
+return true
+}}
+return false
 }
-
-    if (Array.isArray(plugin.command)) {
-      return plugin.command.includes(command)
-}
-
-    return plugin.command === command
-})
-
-  if (existe) return
-
-  const fake = {
-    key: {
-      remoteJid: "status@broadcast",
-      fromMe: false,
-      participant: "0@s.whatsapp.net"
-},
-    message: {
-      liveLocationMessage: {
-        degreesLatitude: 0,
-        degreesLongitude: 0,
-        accuracyInMeters: 0,
-        title: "ɴᴀɢɪ-ʙᴏᴛᴠ𝟷: ᴅᴇᴠ-ꜰᴇᴅᴇxʏᴢ",
-        caption: "ᴄᴀɴᴀʟ ᴏꜰɪᴄɪᴀʟ",
-        sequenceNumber: 1,
-        timeOffset: 0,
-        jpegThumbnail: Buffer.alloc(0)
-}
-}
-}
-
-  await conn.sendMessage(m.chat, {
-    text: `❌ ᴇʟ ᴄᴏᴍᴀɴᴅᴏ *${comandoBuscado}* ɴᴏ ᴇxɪꜱᴛᴇ.\nᴜꜱᴀ *${usedPrefix}ᴍᴇɴᴜ* ᴘᴀʀᴀ ᴠᴇʀ ʟᴀ ʟɪꜱᴛᴀ ᴅᴇ ᴄᴏᴍᴀɴᴅᴏꜱ ᴅɪꜱᴘᴏɴɪʙʟᴇꜱ.`,
-    contextInfo: {
-      externalAdReply: {
-        title: '© ɴᴀɢɪ-ʙᴏᴛᴠ𝟷',
-        body: 'ᴄᴏᴍᴀɴᴅᴏ ɪɴᴠᴀ́ʟɪᴅᴏ',
-        thumbnailUrl: 'https://cdn.yupra.my.id/yp/dpi4ktu8.jpg',
-        mediaType: 1,
-        renderLargerThumbnail: true,
-        sourceUrl: 'https://github.com/hashirama-dev'
-}
-}
-}, { quoted: fake})
-
-  await m.react('❌')
-}
-
-export default handler
+let chat = global.db.data.chats[m.chat]
+let settings = global.db.data.settings[this.user.jid]
+let owner = [...global.owner.map(([number]) => number)].map(v => v.replace(/[^0-9]/g, "") + "@s.whatsapp.net").includes(m.sender)
+if (chat.modoadmin) return
+if (settings.self) return
+if (command === 'mute') return
+if (chat.isMute && !owner) return
+if (command === 'bot') return
+if (chat.isBanned && !owner) return
+if (validCommand(command, global.plugins)) {
+} else {
+const comando = command
+await m.reply(`ꕥ El comando *<${comando}>* no existe.\n> Usa *${usedPrefix}menu, para ver los comandos.`)
+}}
