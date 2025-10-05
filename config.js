bed58cb4b65ce6bb8dbdb43611d6d69ab6a4b756
ᡣ𐭩 𝑫𝒆𝒗-𝒇𝒆𝒅𝒆𝒙𝒚𝒛 🌙 <219152977+Dev-fedexyz17@users.noreply.github.com>
2025-10-05 15:48:57 -0300

    Update config.js

--- config.js
+++ config.js
@@ -79,6 +79,18 @@ global.fetch = fetch
 global.axios = axios
 global.moment = moment   
 
+//✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
+
+global.APIs = {
+xyro: { url: "https://xyro.site", key: null },
+yupra: { url: "https://api.yupra.my.id", key: null },
+vreden: { url: "https://api.vreden.web.id", key: null },
+delirius: { url: "https://api.delirius.store", key: null },
+zenzxz: { url: "https://api.zenzxz.my.id", key: null },
+siputzx: { url: "https://api.siputzx.my.id", key: null }
+}
+
+//✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 
 global.multiplier = 69
 global.maxwarn = '3'
