eb1e1efdaf94511ec6f4d4d5b30adb3a96b5549a
ᡣ𐭩 𝑫𝒆𝒗-𝒇𝒆𝒅𝒆𝒙𝒚𝒛 🌙 <219152977+Dev-fedexyz17@users.noreply.github.com>
2025-10-05 18:51:46 -0300

    Update _on-of.js

--- plugins/_on-of.js
+++ plugins/_on-of.js
@@ -1,93 +1,105 @@
-const handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
-  const fkontak = {
-    key: { participants: '0@s.whatsapp.net', remoteJid: 'status@broadcast', fromMe: false, id: 'Halo' },
-    message: {
-      contactMessage: {
-        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-      }
-    },
-    participant: '0@s.whatsapp.net'
-  };
+const vcard = `BEGIN:VCARD
+VERSION:3.0
+N:;ttname;;;
+FN:ttname
+item1.TEL;waid=13135550002:+1 (313) 555-0002
+item1.X-ABLabel:Celular
+END:VCARD`
 
-  const miniopcion = `🎋 *ON / OFF*
+const qkontak = {
+  key: {
+    fromMe: false,
+    participant: "13135550002@s.whatsapp.net",
+    remoteJid: "status@broadcast"
+},
+  message: {
+    contactMessage: {
+      displayName: "Meta Ai",
+      vcard
+}
+}
+}
+
+const handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner}) => {
+  const opciones = `☕ *ON / OFF*
 
 ${usedPrefix + command} welcome
 ${usedPrefix + command} antilink
 ${usedPrefix + command} nsfw
-${usedPrefix + command} antiprivado`;
+${usedPrefix + command} antiprivado`
 
-  const isEnable = /true|enable|(turn)?on|1/i.test(command);
-  const chat = global.db.data.chats[m.chat];
-  const user = global.db.data.users[m.sender];
-  const bot = global.db.data.settings[conn.user.jid] || {};
-  const type = (args[0] || '').toLowerCase();
-  let isAll = false;
+  const isEnable = /true|enable|(turn)?on|1/i.test(command)
+  const chat = global.db.data.chats[m.chat]
+  const bot = global.db.data.settings[conn.user.jid] || {}
+  const type = (args[0] || '').toLowerCase()
+  let isAll = false
 
-  const validateGroupAdmin = () => {
+  const validarGrupoAdmin = () => {
     if (!m.isGroup) {
       if (!isOwner) {
-        global.dfail('group', m, conn);
-        throw false;
-      }
-    } else if (!isAdmin && !isOwner) {
-      global.dfail('admin', m, conn);
-      throw false;
-    }
-  };
+        global.dfail('group', m, conn)
+        throw false
+}
+} else if (!isAdmin &&!isOwner) {
+      global.dfail('admin', m, conn)
+      throw false
+}
+}
 
-  const validateOwner = () => {
+  const validarOwner = () => {
     if (!isOwner) {
-      global.dfail('owner', m, conn);
-      throw false;
-    }
-  };
+      global.dfail('owner', m, conn)
+      throw false
+}
+}
 
-  const validateROwner = () => {
+  const validarROwner = () => {
     if (!isROwner) {
-      global.dfail('rowner', m, conn);
-      throw false;
-    }
-  };
+      global.dfail('rowner', m, conn)
+      throw false
+}
+}
 
   switch (type) {
     case 'welcome': case 'bienvenida':
-      validateGroupAdmin();
-      chat.welcome = isEnable;
-      break;
+      validarGrupoAdmin()
+      chat.welcome = isEnable
+      break
 
     case 'antilink': case 'antienlace':
-      validateGroupAdmin();
-      chat.antiLink = isEnable;
-      break;
+      validarGrupoAdmin()
+      chat.antiLink = isEnable
+      break
 
     case 'nsfw': case 'nsfwhot': case 'nsfwhorny':
-      validateGroupAdmin();
-      chat.nsfw = isEnable;
-      break;
+      validarGrupoAdmin()
+      chat.nsfw = isEnable
+      break
 
     case 'antiprivado': case 'antiprivate': case 'privado':
-      isAll = true;
-      validateROwner();
-      bot.antiPrivate = isEnable;
-      break;
+      isAll = true
+      validarROwner()
+      bot.antiPrivate = isEnable
+      break
 
     default:
       if (!/[01]/.test(command)) {
-        return conn.reply(m.chat, miniopcion, m, fkontak);
-      }
-      throw false;
-  }
+        return conn.sendMessage(m.chat, {
+          text: opciones,
+...global.rcanal
+}, { quoted: qkontak})
+}
+      throw false
+}
 
-  return conn.reply(
-    m.chat,
-    `⚔️ *La función "${type}" ha sido ${isEnable ? 'activada' : 'desactivada'} ${isAll ? 'en todo el bot' : 'en este chat'}.*`,
-    m,
-    fkontak
-  );
-};
+  return conn.sendMessage(m.chat, {
+    text: `⚔️ *La función "${type}" ha sido ${isEnable? 'activada': 'desactivada'} ${isAll? 'en todo el bot': 'en este chat'}.*`,
+...global.rcanal
+}, { quoted: qkontak})
+}
 
-handler.help = ['enable', 'disable'].map(cmd => `${cmd} <opción>`);
-handler.tags = ['owner', 'group'];
-handler.command = ['enable', 'disable', 'on', 'off'];
+handler.help = ['enable', 'disable'].map(cmd => `${cmd} <opción>`)
+handler.tags = ['owner', 'group']
+handler.command = ['enable', 'disable', 'on', 'off']
 
-export default handler;
+export default handler
